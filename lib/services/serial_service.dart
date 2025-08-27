89025d7a92a962453866f2d98292f99006ce7803
Frendzlu <mateuszfrancik.mtf@gmail.com>
2025-08-27 01:14:54 -0500

    fix

--- lib/services/serial_service.dart
+++ lib/services/serial_service.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:flutter_esp_android_communication/models/message.dart';
@@ -72,55 +73,99 @@ class SerialService {
     }
   }
 
+  (List<Uint8List>, Uint8List) _trySplit(Uint8List data) {
+    List<Uint8List> messageQueue = [];
+    Uint8List currentMessage = data.sublist(0);
+    while (currentMessage.contains(0x0A)) {
+      int index = currentMessage.indexOf(0x0A);
+      Uint8List msg = currentMessage.sublist(0, index);
+      if (index < currentMessage.length-1) {
+        currentMessage = currentMessage.sublist(index+1, currentMessage.length);
+      } else {
+        currentMessage = Uint8List(0);
+      }
+      messageQueue.add(msg);
+    }
+    return (messageQueue, currentMessage);
+  }
+
+
+  // try {
+  //   rx = Message.parse(data, endian: Endian.big);
+  //   logRx("Received ${rx.ack ? "ACK for" : ""} $rx");
+  //   if (rx.command.byte == Command.telemetry.byte) {
+  //     _pointCtl.add(PointWithAuthor(rx.node, rx.points.first));
+  //   }
+  // } on Exception catch (e) {
+  //   logError(e.toString());
+  //   logRx("Raw: ${msg.toString()}");
+  //   logInfo("utf8 representation: ${utf8.decode(msg)}");
+  // }
+
+  (Exception? e, Message? rx) tryParse (Uint8List data) {
+    Message rx;
+    try {
+      rx = Message.parse(data, endian: Endian.big);
+      return (null, rx);
+    } on Exception catch (e) {
+      return (e, null);
+    }
+  }
+
   void _listen() {
     _port!.inputStream?.listen((Uint8List data) {
-      // if (_buf.isNotEmpty) {
-      //   logInfo("Buffer len: ${_buf.length}b, first byte: "
-      //       "${_buf.first.toRadixString(16).padLeft(2, '0')}, last: ${_buf.last.toRadixString(16).padLeft(2, '0')}");
-      // }
-      // logInfo("Data len: ${data.length}b, first byte: "
-      //    "${data.first.toRadixString(16).padLeft(2, '0')}, last: ${data.last.toRadixString(16).padLeft(2, '0')}");
       List<int> bufList = _buf.toList();
       bufList.addAll(data);
       _buf = Uint8List.fromList(bufList);
-      //logRx("buf len: ${_buf.length}");
       logInfo("Raw buf: $_buf");
-      List<Uint8List> messageQueue = [];
-      while (_buf.contains(0x0A)) {
-        int index = _buf.indexOf(0x0A);
-        logInfo("Found line feed at index: $index");
-        Uint8List msg = _buf.sublist(0, index);
-        if (index < _buf.length-1) {
-          _buf = _buf.sublist(index+1, _buf.length);
-        } else {
-          _buf = Uint8List(0);
-        }
-        messageQueue.add(msg);
-      }
+
+      var messageQueueAndLeftover = _trySplit(_buf);
+      List<Uint8List> messageQueue = messageQueueAndLeftover.$1;
+      _buf = messageQueueAndLeftover.$2;
       logInfo("MQ: $messageQueue");
       if (messageQueue.isEmpty) return;
 
-      for (Uint8List msg in messageQueue) {
-        if (msg.first == 0x5B) {
-          try {
-            logRx(utf8.decode(msg));
-          } on Exception catch(e) {
-            logError("Couldn't decode log as UTF8: $e");logInfo("Raw: ${msg.toString()}");
+
+      // TODO: the following code is shit. Please rewrite.
+      Uint8List msg = messageQueue[0].sublist(0);
+      if (msg.first == 0x5B) {
+        try {
+          logRx(utf8.decode(msg));
+        } on Exception catch(e) {
+          logError("Couldn't decode log as UTF8: $e");logInfo("Raw: ${msg.toString()}");
+        }
+        messageQueue.removeAt(0);
+        if (messageQueue.isEmpty) return;
+        msg = messageQueue[0].sublist(0);
+      }
+      var errMsg = tryParse(msg);
+      if (!Message.isValidMessageHeader(msg)) {
+        logError("${errMsg.$1}");
+        logInfo("Raw: $msg");
+        messageQueue.removeAt(0);
+        if (messageQueue.isEmpty) return;
+        msg = messageQueue[0].sublist(0);
+      }
+      while (true) { //holy fucking shit
+        errMsg = tryParse(msg);
+        if (errMsg.$2 != null) {
+          final rx = errMsg.$2!;
+          logRx("Received ${rx.ack ? "ACK for" : ""} $rx");
+          if (rx.command.byte == Command.telemetry.byte) {
+            _pointCtl.add(PointWithAuthor(rx.node, rx.points.first));
           }
+          messageQueue.removeAt(0);
+          if (messageQueue.isEmpty) return;
+          msg = messageQueue[0].sublist(0);
+        } else if (messageQueue.length > 1) {
+          messageQueue.removeAt(0);
+          msg = Uint8List.fromList([...msg, ...messageQueue[0]]);
         } else {
-          try {
-            final rx = Message.parse(msg, endian: Endian.big);
-            logRx("Received ${rx.ack ? "ACK for" : ""} $rx");
-            if (rx.command.byte == Command.telemetry.byte) {
-              _pointCtl.add(PointWithAuthor(rx.node, rx.points.first));
-            }
-          } on Exception catch (e) {
-            logError(e.toString());
-            logRx("Raw: ${msg.toString()}");
-            logInfo("utf8 representation: ${utf8.decode(msg)}");
-          }
+          _buf = Uint8List.fromList([...msg, ..._buf]);
+          return;
         }
       }
+      // end of shit code
     }, onError: (e) {
       logError('Serial error: $e');
       _status('Serial error');
